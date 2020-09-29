9db36168feeb0a0c2d8f1ce4d1da0c2582076d16
Samuel Caro <a20170086@pucp.edu.pe>
2020-09-29 12:52:55 -0500

    Caro

--- app/src/main/java/com/example/lab2g5/MainActivity.java
+++ app/src/main/java/com/example/lab2g5/MainActivity.java
@@ -21,29 +21,29 @@ import java.util.List;
 import android.view.View;
 
 
-
-
 public class MainActivity extends AppCompatActivity {
 
+
+    ArrayList<String> tareas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton botonTelito= findViewById(R.id.botonTelito);
+        ImageButton botonTelito = findViewById(R.id.botonTelito);
 
-        botonTelito.setOnClickListener(new View.OnClickListener(){
+        botonTelito.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 TextView textViewCont = findViewById(R.id.contador);
                 String contadorStr = textViewCont.getText().toString();
                 int contador = Integer.parseInt(contadorStr);
                 contador++;
-             //   if (contador == 6){
+                //   if (contador == 6){
 
 
-               // }
+                // }
                 Log.d("contador", "" + contador);
 
                 textViewCont.setText(String.valueOf(contador));
@@ -53,34 +53,57 @@ public class MainActivity extends AppCompatActivity {
 
         });
         ArrayList<String> prueba = new ArrayList<>();
-        prueba.add("Hola");
-        prueba.add("Adios");
-
         LinearLayout seccionIndustria = (LinearLayout) findViewById(R.id.toDoList);
-        for (String industria : INDUSTRIAS) {
+        for (String tarea : tareas) {
             CheckBox opcion = new CheckBox(this);
-            opcion.setText(industria);
+            opcion.setText(tarea);
             opcion.setLayoutParams(
                     new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             seccionIndustria.addView(opcion);
         }
 
+        TextView textView = findViewById(R.id.textView2);
+        if(tareas.isEmpty()){
+            textView.setVisibility(View.VISIBLE);
+        }
+
 
     }
 
-    @Override
+
+    public void abrirRegistroTarea(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putStringArrayListExtra("tareas", tareas);
+        int requestCode = 1;
+        startActivityForResult(intent, requestCode);
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == 1 && resultCode == RESULT_OK){
-
-            List dataFromMessageActivity = data.getStringArrayListExtra("tareas");
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            ArrayList<String> tareas = data.getStringArrayListExtra("tareas");
+            LinearLayout seccionIndustria = (LinearLayout) findViewById(R.id.toDoList);
+            for (String tarea : tareas) {
+                CheckBox opcion = new CheckBox(this);
+                opcion.setText(tarea);
+                opcion.setLayoutParams(
+                        new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                seccionIndustria.addView(opcion);
+            }
+            TextView textView = findViewById(R.id.textView2);
+            if(tareas.isEmpty()){
+                textView.setVisibility(View.VISIBLE);
+            }else{
+                textView.setVisibility(View.INVISIBLE);
+            }
 
         }
 
     }
 
-    public static String[] INDUSTRIAS ={
+
+    public static String[] INDUSTRIAS = {
             "Arte",
             "Computación",
             "Ingeniería civil",
@@ -89,7 +112,6 @@ public class MainActivity extends AppCompatActivity {
             "Astronomía",
             "Zoología"
     };
-
 
 
 }
